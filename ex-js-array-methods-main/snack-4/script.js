63d0eb2b5f42d036bf205f693cc99ed7f4856bc0
Martina-dinapoli98 <martinadinapoli1998@gmail.com>
2025-02-26 16:36:23 +0100

    add snack 4

--- ex-js-array-methods-main/snack-4/script.js
+++ ex-js-array-methods-main/snack-4/script.js
@@ -3,7 +3,12 @@ const posts = [
   { author: 'Luca', date: '30/6/2021', content: 'lorem ipsum...' },
   { author: 'Fabrizio', date: '12/9/2020', content: 'lorem ipsum...' },
   { author: 'Enrico', date: '09/12/2021', content: 'lorem ipsum...' },
-]
+];
+
+posts.forEach((element)=>{
+  console.log(element.author);
+  
+})
 
 // Crea un nuovo array coi nomi di tutti gli autori
 // Risultato: ['Marco', 'Luca', 'Fabrizio', 'Enrico']
