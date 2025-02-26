d16522098c45ed5835ec00dde6dea6b2e9fbe7b9
Martina-dinapoli98 <martinadinapoli1998@gmail.com>
2025-02-26 15:59:12 +0100

    add 1 snack

--- ex-js-array-methods-main/snack-1/script.js
+++ ex-js-array-methods-main/snack-1/script.js
@@ -1,4 +1,9 @@
 const names = ['Edoardo', 'Simone', 'Francesco'];
 
+
+names.forEach((names)=>{
+    console.log(names);
+    
+})
 // Stampa in console tutti i nomi
 // Risultato: 'Edoardo', 'Simone', 'Francesco'
