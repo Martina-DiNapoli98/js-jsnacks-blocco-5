67a93afb8e446672925d50d4cb47b4697655675a
Martina-dinapoli98 <martinadinapoli1998@gmail.com>
2025-02-26 17:05:33 +0100

    add snack 7

--- ex-js-array-methods-main/snack-7/script.js
+++ ex-js-array-methods-main/snack-7/script.js
@@ -19,5 +19,16 @@ const students = [
   },
 ];
 
+let student = students.find((person)=>{
+ return person.id === 2
+ 
+})
+console.log(student);
+
+
+
+
+
+
 // Recupera i dati dello studente con id 2
 // Risultato: { id: 2, name: 'Mario Banfi', age: 34, class: '4A' }
