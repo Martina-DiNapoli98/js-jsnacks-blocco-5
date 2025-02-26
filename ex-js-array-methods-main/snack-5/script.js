2ff770ad9f922c3e1618cd5d44a285d4bbe62606
Martina-dinapoli98 <martinadinapoli1998@gmail.com>
2025-02-26 16:39:05 +0100

    add snack 5

--- ex-js-array-methods-main/snack-5/script.js
+++ ex-js-array-methods-main/snack-5/script.js
@@ -1,4 +1,12 @@
 const nums = [2, 8, 4, 7, 12, 87];
 
+let pari = nums.filter((num)=>{
+    if(num % 2 === 0){
+        return true
+    } return false
+})
+console.log(pari);
+
+
 // Crea un array a partire dall'array nums, che abbia solo i numeri pari
 // Risultato: [2, 8, 4, 12]
